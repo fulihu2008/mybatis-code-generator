38099512b89d061b41866a72fc3e782a4c9c9c3a
fulh <fulh@asiainfo.com>
2019-09-11 19:32:58 +0800

    init

---
+++ src/main/java/com/fulh/dao/UserDOMapper.java
@@ -0,0 +1,17 @@
+package com.fulh.dao;
+
+import com.fulh.dataobject.UserDO;
+
+public interface UserDOMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(UserDO record);
+
+    int insertSelective(UserDO record);
+
+    UserDO selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(UserDO record);
+
+    int updateByPrimaryKey(UserDO record);
+}
